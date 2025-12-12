c16cb5a1e4547ef99b7a70638e8ceed5f00336b5
Vladimir Pavlyuk <vladvic@gmail.com>
2025-12-13 03:21:16 +0400

    Add SettingsHolder instead of Settings

--- include/util/Settings.hpp
+++ include/util/Settings.hpp
@@ -12,74 +12,210 @@
 #include <map>
 #include <string>
 #include <variant>
+#include <vector>
 
 #include "Singleton.hpp"
 
 namespace util
 {
 
-class Settings
-  : public Singleton<Settings>
+template<typename T>
+struct is_pair
+  : public std::false_type
+{ };
+
+template<typename A, typename B>
+struct is_pair<std::pair<A, B>>
+  : public std::true_type
 {
-  using ValueType = std::variant<int64_t, uint64_t, std::string, double>;
-  std::map<std::string, ValueType> m_values;
+  using FirstType  = A;
+  using SecondType = B;
+};
+
+template<typename T>
+constexpr bool is_pair_v = is_pair<T>::value;
+
+class SettingsValue
+{
+  using KeyType   = std::string;
+  using ValueType = std::variant<std::nullptr_t, 
+                                 int64_t, 
+                                 uint64_t, 
+                                 std::string, 
+                                 double, 
+                                 std::vector<SettingsValue>, 
+                                 std::map<KeyType, SettingsValue>>;
+
+  ValueType m_value;
 
 public:
+  void resize(size_t s) {
+    if (!(*this)) {
+      m_value = std::vector<SettingsValue>{};
+    }
+    std::get<std::vector<SettingsValue>>(m_value).resize(s);
+  }
+
+  size_t size() {
+    return std::visit(
+      [](auto &t) {
+        using T = std::decay_t<decltype(t)>;
+        if constexpr (std::is_same_v<T, std::vector<SettingsValue>> ||
+                      std::is_same_v<T, std::map<KeyType, SettingsValue>>)
+        {
+          return (size_t)t.size();
+        }
+        return (size_t)0;
+      }, m_value);
+  }
+
+  template<typename Callable>
+  void forEach(Callable&& callback) {
+    std::visit(
+      [&callback](auto &t) {
+        using T = std::decay_t<decltype(t)>;
+        if constexpr (std::is_same_v<T, std::vector<SettingsValue>>) {
+          size_t i = 0;
+          for (auto &e : t) {
+            std::pair<size_t, SettingsValue&> v(i++, e);
+            callback(v);
+          }
+        }
+        else if constexpr (std::is_same_v<T, std::map<KeyType, SettingsValue>>) {
+          for (auto &v : t) {
+            callback(v);
+          }
+        }
+        else {
+            callback(t);
+        }
+      }, m_value);
+  }
+
+  std::vector<KeyType> keys() {
+    std::vector<KeyType> keys;
+    if (!(*this)) {
+      m_value = std::map<KeyType, SettingsValue>{};
+    }
+    auto &map = std::get<std::map<KeyType, SettingsValue>>(m_value);
+    for (auto &p : map) {
+      keys.push_back(p.first);
+    }
+    return keys;
+  }
+
+  operator bool() const {
+    return std::visit(
+      [](auto &t) {
+        using T = std::decay_t<decltype(t)>;
+        if constexpr (std::is_same_v<T, std::nullptr_t>) {
+          return false;
+        }
+        return true;
+      }, m_value);
+  }
+
+  template<typename T>
+  requires std::is_integral_v<T>
+  SettingsValue &operator[](T i) {
+    if (!(*this)) {
+      m_value = std::vector<SettingsValue>{};
+    }
+    return std::get<std::vector<SettingsValue>>(m_value)[i];
+  }
+
+  template<typename T>
+  requires std::is_constructible_v<KeyType, T>
+  SettingsValue &operator[](const T &i) {
+    if (!(*this)) {
+      m_value = std::map<KeyType, SettingsValue>{};
+    }
+    return std::get<std::map<KeyType, SettingsValue>>(m_value)[i];
+  }
+
+  const SettingsValue &operator[](size_t i) const {
+    return std::get<std::vector<SettingsValue>>(m_value)[i];
+  }
+
+  const SettingsValue &operator[](const KeyType &i) const {
+    return std::get<std::map<KeyType, SettingsValue>>(m_value).at(i);
+  }
+
   template<typename T>
   requires std::is_signed_v<T> && std::is_integral_v<T>
-  T get(const std::string &s)
-  {
-    return std::get<int64_t>(m_values.at(s));
+  T as() const {
+    return std::get<int64_t>(m_value);
   }
 
   template<typename T>
   requires std::is_unsigned_v<T> && std::is_integral_v<T>
-  T get(const std::string &s)
-  {
-    return std::get<uint64_t>(m_values.at(s));
+  T as() const {
+    return std::get<uint64_t>(m_value);
   }
 
   template<typename T>
   requires std::is_constructible_v<T, const char*>
-  T get(const std::string &s)
-  {
-    return T(std::get<std::string>(m_values.at(s)).c_str());
+  T as() const {
+    return T(std::get<std::string>(m_value).c_str());
   }
 
   template<typename T>
   requires std::is_floating_point_v<T>
-  T get(const std::string &s)
-  {
-    return std::get<double>(m_values.at(s));
+  T as() const {
+    return std::get<double>(m_value);
+  }
+
+  template<typename T, typename N>
+  requires std::is_same_v<T, std::vector<N>>
+  T as() const {
+    return std::get<T>(m_value);
   }
 
   template<typename T>
   requires std::is_signed_v<T> && std::is_integral_v<T>
-  void set(const std::string &s, const T &v)
-  {
-    m_values[s] = (int64_t)v;
+  void set(const T &v) {
+    m_value = (int64_t)v;
   }
 
   template<typename T>
   requires std::is_unsigned_v<T> && std::is_integral_v<T>
-  void set(const std::string &s, const T &v)
-  {
-    m_values[s] = (uint64_t)v;
+  void set(const T &v) {
+    m_value = (uint64_t)v;
   }
 
   template<typename T>
   requires std::is_constructible_v<std::string, const T&>
-  void set(const std::string &s, const T &v)
-  {
-    m_values[s] = std::string(v);
+  void set(const T &v) {
+    m_value = std::string(v);
   }
 
   template<typename T>
   requires std::is_floating_point_v<T>
-  void set(const std::string &s, const T &v)
-  {
-    m_values[s] = (double)v;
+  void set(const T &v) {
+    m_value = (double)v;
   }
+
+  template<typename T>
+  SettingsValue &operator=(const T &v) {
+    set<T>(v);
+    return *this;
+  }
+};
+
+class SettingsHolder
+{
+  using KeyType   = std::string;
+  using ValueType = SettingsValue;
+  std::map<KeyType, ValueType> m_values;
+
+public:
+  auto begin() { return m_values.begin(); }
+  auto end() { return m_values.begin(); }
+  auto begin() const { return m_values.begin(); }
+  auto end() const { return m_values.begin(); }
+  ValueType &operator[](const KeyType &k) { return m_values[k]; }
+  const ValueType &operator[](const KeyType &k) const { return m_values.at(k); }
+  ValueType &at(const KeyType &k) { return m_values.at(k); }
 };
 
 } // namespace util
--- tests/util/main.cpp
+++ tests/util/main.cpp
@@ -12,19 +12,35 @@
 
 int main()
 {
-  util::Settings settings;
+  util::SettingsHolder settings;
 
-  settings.set("valueA", std::string("Hello"));
-  settings.set("valueB", "World");
-  settings.set("valueC", 42);
-  settings.set("valueD", -42);
-  settings.set("valueE", (unsigned int)42);
-  settings.set("valueF", 0.123);
+  settings["valueA"] = std::string("Hello");
+  settings["valueB"] = "World";
+  settings["valueC"] = 42;
+  settings["valueD"] = -42;
+  settings["valueE"] = (unsigned int)42;
+  settings["valueF"] = 0.123;
+  settings["Section1"]["valueG"] = 1234;
+  settings["Section1"]["array"].resize(5);
+  settings["Section1"]["array"][0] = 1;
+  settings["Section1"]["array"][1] = 2;
+  settings["Section1"]["array"][2] = 3;
+  settings["Section1"]["array"][3] = 4;
+  settings["Section1"]["array"][4] = 5;
 
-  std::cout << "valueA: " << settings.get<std::string>("valueA") << std::endl;
-  std::cout << "valueB: " << settings.get<const char*>("valueB") << std::endl;
-  std::cout << "valueC: " << settings.get<int>("valueC") << std::endl;
-  std::cout << "valueD: " << settings.get<int>("valueD") << std::endl;
-  std::cout << "valueE: " << settings.get<unsigned int>("valueE") << std::endl;
-  std::cout << "valueF: " << settings.get<float>("valueF") << std::endl;
+  std::cout << "valueA: " << settings["valueA"].as<std::string>() << std::endl;
+  std::cout << "valueB: " << settings["valueB"].as<const char*>() << std::endl;
+  std::cout << "valueC: " << settings["valueC"].as<int>() << std::endl;
+  std::cout << "valueD: " << settings["valueD"].as<int>() << std::endl;
+  std::cout << "valueE: " << settings["valueE"].as<unsigned int>() << std::endl;
+  std::cout << "valueF: " << settings["valueF"].as<float>() << std::endl;
+  std::cout << "valueG: " << settings["Section1"]["valueG"].as<int>() << std::endl;
+  settings["Section1"]["array"].forEach(
+    [](auto &v) {
+      using T = std::decay_t<decltype(v)>;
+      if constexpr (util::is_pair_v<T>) {
+        util::SettingsValue &val = v.second;
+        std::cout << "array[" << v.first << "]: " << (val.as<int>()) << std::endl;
+      }
+    });
 }
